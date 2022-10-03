6384bfd2b7a84be4ac3c15cee1114289f6d40e6c
Melissa Howard <how20034@byui.edu>
2022-10-03 11:58:15 -0600

    added project structure, main run file, and game loop skeleton code

---
+++ hilo/game_classes/game_loop.py
@@ -0,0 +1,54 @@
+from game_classes.player import Player
+from game_classes.card import Card
+
+class Game_loop:
+    """The main game play loop.
+
+        responsibility: to control the order of game play.
+
+        attributes:
+            cards (list of integers[Card]):  a list of two card instances
+            player (Player): instance of a player
+
+    """
+    def __init__(self):
+        """Constructs an new game loop.
+        
+        args: self (Game_loop): an instance of Game_loop.
+        """
+        self.cards = []
+        self.player = Player()
+
+        for i in range(2):
+            card = Card()
+            self.cards.append(card)
+
+    def start_game(self):
+        play = "y" #establishes a variable to track the game loop iterations
+
+        self.player.display_points() #displays player's starting points
+
+        while play == "y":
+            card = Card() #establishes a new card value for the next game loop
+
+            print (f"The card is: {self.cards[0].display()}") # displays first card value, possibly replace with self.cards[0].display1() Card class method
+
+            guess = self.player.input() #Player class method asking for player input
+
+            print (f"Next card was: {self.cards[1].display()}") # displays first card value, possibly replace with self.cards[0].display2() Card class method
+
+            self.player.point_update(self.cards) #Player class method to update points based on player guess
+            
+            self.player.display_points() #player method to display player's updated points
+            
+            ####These two functions can be combined into a single function defined as a Game_loop class method -- it updates the cards list to prepare for the next games round
+            self.cards.pop(0) #removes first card in Cards list
+            self.cards.append(card) #adds a new card to the end of the list
+
+            if self.player.points <= 0:
+                print ("I'm sorry you're out of points")
+                play = "n"
+            else:
+                play = input("Play again? [y/n]")
+
+        print("Thanks for Playing!")
---
+++ hilo/game_classes/player.py
@@ -0,0 +1,14 @@
+''' Player Class
+
+    responsibility: to collect and hold player guess, hold and display player points
+    
+    attributes: player guess (an integer)
+                points (an integer, starts at 300 and updates based on guess compared to card
+                       relationship)
+    
+    methods: player input (collect the player's guess, convert to integer)
+             update points (compare guess to card relationship takes the argument
+                of a list of two integers)
+             display points (display the new updated points)
+             
+'''
