e8c83b7bce356f14a8ee1c46cf462911fef306bc
Melissa Howard <how20034@byui.edu>
2022-10-05 14:29:27 -0600

    card class update

--- hilo/game_classes/card.py
+++ hilo/game_classes/card.py
@@ -7,3 +7,17 @@
    methods: get_value (returns the value of the card)
 
 '''
+class Card: 
+          value: [“1”, “2”, “3”, “4”, “5”, “6”, “7”, “8”, “9”, “10”, “11”, “12”, “13”,]
+      def __init__(self, value):
+         sef.value= value 
+      def __str__(self):
+         return f”{Card.value[self.value]}”
+
+class Card:
+
+   def __init__(self):
+      self.value = random.randint(1, 13)
+   
+   def get_value(self):
+         return self.value
